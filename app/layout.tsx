878a769dddacef4b913a92f4d90246b0b72fb7c3
ahmadsohail404 <sohailss2412@gmail.com>
2025-08-19 15:32:16 +0530

    favicon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,22 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Jyesta Campus Executive",
   description: "Become the official face of Jyesta in your college.",
+  icons: {
+    icon: [
+      {
+        url: "/favicons/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/favicons/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    apple: "/favicons/android-chrome-192x192.png",
+    shortcut: "/favicons/android-chrome-512x512.png",
+  },
 };
 
 export default function RootLayout({
